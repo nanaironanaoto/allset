2fa3a7e8054e67e31fd012ceb6622823dd553f9a
nanaironanaoto <nanaironanaoto@gmail.com>
2023-02-01 20:00:45 +0900

    commit

--- parallax/script.js
+++ parallax/script.js
@@ -25,3 +25,34 @@ window.addEventListener('scroll', function () {
     $('#vertical_textbox_bnr03').css('opacity', scroll_stop_and_fade03);
   }
 })
+
+
+
+//パスワード認証 ( pass : test1234 )
+document.body.style.display = 'none';
+window.onload = function () {
+  var UserInput = null;
+
+  var cookies = document.cookie;
+  var cookiesArray = cookies.split(';');
+  var pass = 'dGVzdDEyMzQ=';
+
+  for (var c of cookiesArray) {
+    var cArray = c.split('=');
+    if (cArray[0].indexOf('cruw-basic') > -1) {
+      UserInput = decodeURIComponent(cArray[1]);
+    }
+  }
+  if (!(UserInput && UserInput == window.atob(pass))) {
+    UserInput = prompt("パスワードを入力して下さい:", "");
+  }
+
+  if (UserInput != window.atob(pass)) {
+    document.body.innerHTML = "403 Forbidden";
+  } else {
+    var now = new Date();
+    now.setMinutes(now.getMinutes() + 60 * 24 * 3);
+    document.cookie = "cruw-basic=" + encodeURIComponent(UserInput) + ";expires=" + now.toUTCString() + "; path=/;";
+  }
+  document.body.style.display = null;
+}
